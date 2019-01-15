6dfccd990251ed1388ff40574846cc34d8555aef
ANSOOOH <0308su@naver.com>
2019-01-15 15:49:23 +0900

    first django

---
+++ webboard/board/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.Foreighkey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DataTimeField(
+        default=timezone.now
+    )
+
+    published_data = models.DataTimeField(
+        blank=True, null=True
+    )
+
+    def publish(self):
+        self.published_data = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
